b9813b2f010ca0b305b7ae0a0e21c20f34fd91fc
Emilia <115167614+emiliagadeeva@users.noreply.github.com>
2025-12-07 13:33:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 // config.js
 
-// ⚠️ ВАЖНО: сюда ставишь ПРЯМЫЕ ссылки для скачивания df.csv и wine_embeddings.json
-// Формат для Google Drive: https://drive.google.com/file/d/1KMy_lZIziIsGI3SE2EInydfZJ6rPWlIE/view?usp=drive_link
-//   
+// CSV из второго линка:
+window.CSV_URL = "https://drive.google.com/uc?export=download&id=18mwRZRlY3f6M6nN6VmiHKzDAAZxfEF7A";
 
-const CSV_URL = "https://drive.google.com/uc?export=download&id=18mwRZRlY3f6M6nN6VmiHKzDAAZxfEF7A";
-const EMBEDDINGS_URL = "https://drive.google.com/uc?export=download&id=1KMy_lZIziIsGI3SE2EInydfZJ6rPWlIE";
+// JSON с эмбеддингами из первого линка:
+window.EMBEDDINGS_URL = "https://drive.google.com/uc?export=download&id=1KMy_lZIziIsGI3SE2EInydfZJ6rPWlIE";
 
-// Сколько рекомендаций показывать по умолчанию
-const DEFAULT_TOP_K = 10;
+// Сколько рекомендаций показывать
+window.DEFAULT_TOP_K = 10;
